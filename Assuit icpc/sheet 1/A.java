059b8ba6214c402d21cb76f83a07d1210d105a04
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-14 18:40:07 +0200

    A

--- Assuit icpc/sheet 1/A.java
+++ Assuit icpc/sheet 1/A.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class A{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("what is name ?");
         String name = sc.next();
         System.out.println("Hello, " + name);
     }
-}
+}   
