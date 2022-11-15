b70a0f2152bae07e55bfb8f259e945ef26b7ff36
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-15 02:10:58 +0200

    A.Problem Solution

--- Assuit icpc/sheet 1/A.java
+++ Assuit icpc/sheet 1/A.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+
 public class A{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+ 
+        String name ;
         System.out.println("Hello, " + name);
     }
 }   
