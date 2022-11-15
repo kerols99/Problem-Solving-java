c447ab2c5127e05536c53d735f6c766cb96499d9
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-15 03:16:36 +0200

    D.Problem Solution

--- Assuit icpc/sheet 1/D.java
+++ Assuit icpc/sheet 1/D.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class D {
     public static void main(String[] args){
-        long A = 
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        long D = sc.nextLong();
+        System.out.println("Difference = " + ((A*B)-(C*D)));
     }
 }
