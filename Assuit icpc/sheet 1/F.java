940a53f753d415689f917a5478cc20070b18bf99
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-15 06:25:19 +0200

    F.Problem Solution

---
+++ Assuit icpc/sheet 1/F.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class F {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+            long N = (sc.nextLong()%10);
+            long M = (sc.nextLong()%10);
+            System.out.println(N+M);
+    }
+}
