de63800e09678d0cd11ae6a655debbcbc1e417d7
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-15 02:12:15 +0200

    B.Problem Solution

---
+++ Assuit icpc/sheet 1/B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class B{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        long l = sc.nextLong();
+        char ch = sc.next().charAt(0);
+        float fl = sc.nextFloat();
+        double d = sc.nextDouble();
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(ch);
+        System.out.println(fl);
+        System.out.println(d);
+    }
+}
