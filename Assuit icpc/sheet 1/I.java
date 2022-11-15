facca68b9e132ac3ec1cb4db69d3f1006b914b4f
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-16 01:54:45 +0200

    I.Problem Solution

---
+++ Assuit icpc/sheet 1/I.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if(A>=B){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
