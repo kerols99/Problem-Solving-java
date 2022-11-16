9bc961adfa86c128c2f406e89c537c23ad215e39
kerols99 <93666440+kerols99@users.noreply.github.com>
2022-11-16 02:20:44 +0200

    J.Problem Solution

---
+++ Assuit icpc/sheet 1/J.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class J {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if(A%B == 0 || B%A == 0){
+            System.out.println("Multiples");
+        }else{
+            System.out.println("No Multiples");
+        }
+    }
+}
